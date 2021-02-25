a886db1031f76386abc7fc77f3d5a0344b6db046
Horea Oros <horea.oros@gmail.com>
2021-02-25 11:48:43 +0200

    add bars project

---
+++ Bars/Ladder.cs
@@ -0,0 +1,26 @@
+﻿namespace Bars
+{
+    // TODO
+
+    /// <summary>
+    /// +---+
+    /// |
+    /// |
+    /// +---+
+    /// |
+    /// |
+    /// +---+
+    /// |
+    /// |
+    /// +---+
+    /// </summary>
+    internal class Ladder
+    {
+        
+
+        public Ladder(int width, int height)
+        {
+
+        }
+    }
+}
---
+++ Bars/VertBar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bars
+{
+    internal class VertBar
+    {
+        private int height;
+
+        public VertBar(int height)
+        {
+            this.height = height;
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
